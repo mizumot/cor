62c8a52fce0709ac9c085c9686f9bfb2fb3d9ca2
Atsushi Mizumoto <atsushi@mizumot.com>
2013-12-23 00:27:14 +0900

    Update server.R

--- server.R
+++ server.R
@@ -44,36 +44,47 @@ shinyServer(function(input, output) {
     })
     
     
+    
+    makecorPlot <- function(){
+        if (input$colname == 0) {
+            x <- read.table(text=input$text, sep="\t")
+            x <- as.matrix(x)
+            
+            type <- switch(input$method,
+            Pearson = "pearson",
+            Spearman = "spearman",
+            Kendall = "kendall")
+            
+            pairs.panels(x, method = type)
+            
+        } else {
+            x <- read.csv(text=input$text, sep="\t")
+            
+            type <- switch(input$method,
+            Pearson = "pearson",
+            Spearman = "spearman",
+            Kendall = "kendall")
+            
+            pairs.panels(x, method = type)
+        }
+    }
+    
     output$corPlot <- renderPlot({
-       
-       
-       if (input$colname == 0) {
-           x <- read.table(text=input$text, sep="\t")
-           x <- as.matrix(x)
-           
-           type <- switch(input$method,
-                          Pearson = "pearson",
-                          Spearman = "spearman",
-                          Kendall = "kendall")
-           
-           pairs.panels(x, method = type)
-           
-       } else {
-           x <- read.csv(text=input$text, sep="\t")
-           
-           type <- switch(input$method,
-                          Pearson = "pearson",
-                          Spearman = "spearman",
-                          Kendall = "kendall")
-           
-           pairs.panels(x, method = type)
-       }
+        print(makecorPlot())
     })
     
-
-
-
-
+    
+    
+    info <- reactive({
+        info1 <- paste("This analysis was conducted with ", strsplit(R.version$version.string, " \\(")[[1]][1], ".", sep = "")# バージョン情報
+        info2 <- paste("It was executed on ", date(), ".", sep = "")# 実行日時
+        cat(sprintf(info1), "\n")
+        cat(sprintf(info2), "\n")
+    })
+    
+    output$info.out <- renderPrint({
+        info()
+    })
 
 
 
@@ -84,6 +95,17 @@ shinyServer(function(input, output) {
     output$correl.out <- renderPrint({
         correl()
     })
+    
+    output$downloadCorPlot <- downloadHandler(
+    filename = function() {
+        paste('Corplot-', Sys.Date(), '.pdf', sep='')
+    },
+    content = function(FILE=NULL) {
+        pdf(file=FILE)
+		print(makecorPlot())
+		dev.off()
+	})
+
     
 
 })
